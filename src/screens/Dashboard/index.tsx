113184f9fe94b29e445d4fea644ba310851fe298
ABNER SOUSA LIMA <120082@pmenos.com.br>
2022-04-17 18:02:48 -0300

    Criação dos cartões de destaque

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  Photo,
+  User,
+  UserGreeting,
+  UserInfo,
+  UserName,
+  Icon
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/67610973?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá ,</UserGreeting>
+              <UserName>Abner</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>
+      <HighlightCard />
+    </Container>
+  );
+}
